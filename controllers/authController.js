cff431458669ed7e4bd7e43ec218789a058c5e61
Greg Gancarz <propterprivatum@tuta.io>
2019-06-03 15:23:54 -0500

    style: Added more semantic comments and generic notifications in the console.log

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,8 +2,10 @@ const express	 = require('express');
 const router     = express.Router();
 const User		 = require('../models/user');
 const bcrypt 	 = require('bcrypt');
-const Category = require('../models/category');
 
+// email: fakestEmailEver@dne.com
+// password: password1
+// _id: 5cf56a9fda6f2733f4e15b2b
 
 //////////// REGISTER / LOGIN / LOGOUT ///////////
 
@@ -100,26 +102,7 @@ router.get('/:id', async (req, res, next) => {
 	};
 });
 
-router.post('/:id', async (req, res, next) => {
-	try {
-		foundUser = await User.findById({_id: req.params.id});
-		console.log(req.body, "<<<< req.body");
-		const catName = req.body
-		const createdCat = await Category.create(req.body);
-		console.log(createdCat, "<<< The createdCat");
-		console.log(foundUser.categories, "<<<< foundUser.categories BEFORE push");
-		foundUser.categories.push(createdCat);
-		console.log(foundUser.categories, "<<<< foundUser.categories AFTER push");
-
-		res.json({
-			status: 200,
-			data: createdCat,
-		});
-	} catch(err) {
-		next(err);
-	};
-});
-
+/////////// PROFILE PAGES /// ABOVE ///////////
 
 
 
--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,20 +3,18 @@ const router = express.Router();
 const User = require('../models/user');
 const Category = require('../models/category');
 
-
+// email: fakestEmailEver@dne.com
+// password: password1
+// _id: 5cf56a9fda6f2733f4e15b2b
 
 router.post('/:id', async (req, res, next) => {
+	console.log("--Category creation has been initiated---");
 	try {
 		foundUser = await User.findById({_id: req.params.id});
-		console.log(req.body, "<<<< req.body");
 		const catName = req.body
 		const createdCat = await Category.create(req.body);
-		console.log(createdCat, "<<< The createdCat");
-		console.log(foundUser.categories, "<<<< foundUser.categories BEFORE push");
 		foundUser.categories.push(createdCat);
 		foundUser.save();
-		console.log(foundUser.categories, "<<<< foundUser.categories AFTER push");
-
 		res.json({
 			status: 200,
 			data: createdCat,
@@ -29,6 +27,3 @@ router.post('/:id', async (req, res, next) => {
 module.exports = router;
 
 
-// email: fakestEmailEver@dne.com
-// password: password1
-// _id: 5cf56a9fda6f2733f4e15b2b
